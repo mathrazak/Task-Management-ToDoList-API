e86e151283aefb617db962c4978578f7609261b9
mathrazak <matheus.sobreiro.fdsm@gmail.com>
2026-05-10 16:36:24 -0300

    adjust task model

--- src/main/java/br/com/mathrazak/todolist/task/TaskModel.java
+++ src/main/java/br/com/mathrazak/todolist/task/TaskModel.java
@@ -42,5 +42,12 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if(title.length() > 50){
+            throw new Exception("O campo deve conter no máximo 50 caracteres!");
+        }
+        this.title = title;
+    }
     
 }
